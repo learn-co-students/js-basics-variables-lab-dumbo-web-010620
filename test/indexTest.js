a4a8bf90d80df2807196f9310b9ff4c3567eb0a1
github username <github email address>
2020-02-20 10:38:12 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,7 +7,7 @@ const babel = require('babel-core');
 
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
-
+ 
 
 describe('index.js', function () {
   describe('companyName', function () {
